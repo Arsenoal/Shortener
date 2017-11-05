8173b2125b76f106665765ef973222c785ebf097
Arsen <arsen.vudumyan@gmail.com>
2017-11-05 21:08:37 +0400

    file first commit.

---
+++ Solution.java
@@ -0,0 +1,14 @@
+package shorterner;
+
+import shorterner.strategy.OurHashBiMapStorageStrategy;
+import shorterner.strategy.StorageStrategy;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+}
